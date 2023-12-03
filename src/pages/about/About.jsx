d5e7cbe20c6af9c98426afd904cba283ac7a8151
Stevetech1 <steveojata54@gmail.com>
2023-12-03 20:45:53 +0100

    updated

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,17 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/MITS1.jpg'
 import './about.css'
 
 
 
 const About = () => {
   return (
-    <div>About</div>
+   <>
+   <Header title="About Us" image={HeaderImage}>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, dolore quibusdam ipsum deserunt, ullam voluptate maiores deleniti 
+    aut temporibus explicabo minus reiciendis, in tempora ex distinctio asperiores necessitatibus blanditiis vel.
+   </Header>
+   </>
   )
 }
 
